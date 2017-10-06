c9a2a2874b134ea33d1c173e4b4e039feddbee4a
ronaldmpd <admin@example.com>
2017-10-05 23:29:29 -0400

    Clean source ...

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-/*
-Module animal
-//import { Animal } from "./model/animal/animal"; 
-//import { Gato } from "./model/animal/gato"; 
-//import { Guepardo } from "./model/animal/guepardo"; 
-import { Animal, Gato, Guepardo } from "./model/animal/animal-module"; 
-
-import { sayHello } from "./model/animal/funtions"; 
-
-let gato: Animal = new Gato('Misifu');
-let guepardo: Guepardo = new Guepardo('Corredor');
-
-gato.mover(2);
-guepardo.mover();
-
-gato.nombre = 'to';
-gato.mover();
-console.log("gato.nombre: " + gato.nombre);
-
-console.log('sayHello:' + sayHello());
-*/
 console.log('Modulo BANCO');
 
 import { Direccion,  Cliente, CajaDeAhorro, CuentaCorriente } from "./model/banco/banco-module"; 
@@ -26,8 +5,6 @@ import { Direccion,  Cliente, CajaDeAhorro, CuentaCorriente } from "./model/banc
 let direccion: Direccion = new Direccion('Bol', 'Cbba', 'Calle A');
 let cliente : Cliente = new Cliente(1,'cuentaA');
 cliente.direccion = direccion;
-//console.log('direccion', direccion);
-//console.log('cliente', cliente);
 
 let cajaDeAhorroBs : CajaDeAhorro = new CajaDeAhorro(1000, 220000, 'Bs', 3.5);
 let cajaDeAhorroDolar : CajaDeAhorro = new CajaDeAhorro(1001, 5000, 'Dolares', 2.5);
